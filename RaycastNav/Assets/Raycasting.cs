6d3a6642f11efd403a4068cbf924031a0be46e37
Conner Anderson <connerwithahat@gmail.com>
2020-04-01 23:35:24 -0700

    Added Unity Project

---
+++ RaycastNav/Assets/Raycasting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Raycasting : MonoBehaviour
+{
+    public Text text;
+    public Camera camera;
+    public float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            if (hit.collider.gameObject.tag == "Ball")
+            {
+                text.text = "Press 'E' to Interact";
+                if (Input.GetKeyDown("e"))
+                {
+                    //If you're reading this without actually playing, April Fools, ya goober
+                    System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+                }
+            }
+        }
+        else
+        {
+            text.text = "";
+        }
+    }
+}
